8b57e94b93616df25598428aa5de0ed21d78886d
changjinpeng <24303312@qq.com>
2021-02-24 10:16:52 +0800

    第二次提交

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div id="app">
     <router-view />
-    <h1>黑马头条</h1>
-    <i class="iconfont iconlishi"></i>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 <script>
---
+++ src/views/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login-container">
+    <van-nav-bar title="登录" />
+    <van-form>
+      <van-field name="用户名" placeholder="用户名" />
+      <van-field type="password" name="密码" placeholder="密码" />
+      <div style="margin: 16px;">
+        <van-button block type="info" native-type="submit">
+          提交
+        </van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage'
+}
+</script>
+<style scoped lang="less"></style>
